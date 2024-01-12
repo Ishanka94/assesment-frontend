9a9493faefb8c19be50e73e4e1c3003b1c38aff2
Ishanka94 <tharindu.ishanka1994@gmail.com>
2024-01-12 12:26:37 +0400

    Auth related changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/shared/Layout';
 import Dashboard from './pages/Dashboard';
-import Reviews from './pages/Reviews'
+import Reviews from './pages/Reviews';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
             <Routes>
+                <Route path="/login" element={<Login />} />
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Dashboard />} />
                     <Route path="reviews" element={<Reviews />} />
